cf53454aa6e9908c1eaa7d85c6a8c371acedce35
Deepa <kosalai17@gmail.com>
2021-06-29 17:59:28 -0700

    player function and object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,6 +1,8 @@
 const Player = require('../lib/Player.js');
-const Potion =require('../lib/Potion');
-jest.mock('../lib/Potion');
+const Potion =require('../lib/__mock__/Potion');
+jest.mock('../lib/Potion.js');
+console.log(new Potion());
+
 
 test('create a player object',()=>{
     const player =new Player('Dave')
@@ -11,3 +13,20 @@ test('create a player object',()=>{
     expect(player.agility).toEqual(expect.any(Number));
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
 });
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+  
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+  });
+  test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
+  });
